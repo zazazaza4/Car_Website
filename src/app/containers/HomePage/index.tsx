1309dea4e4cdcf52aeeec0849bab47df7ca9d248
Yaroslav <6715414@gmail.com>
2023-02-05 17:05:46 +0200

    added HomePage with TopSection and Navbar components

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Navbar } from "../../components/navbar";
+
+import { TopSection } from "./topSection";
+
+const PageContainer = styled.div`
+  ${tw`
+  flex 
+  flex-col 
+  h-full 
+  w-full 
+  items-center 
+  overflow-x-hidden
+  `}
+`;
+
+export function HomePage() {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+    </PageContainer>
+  );
+}
